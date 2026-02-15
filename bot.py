d0f3f66a91fd4af3753f04d14843a45be37a7e28
Robbins-je <103512215+Robbins-je@users.noreply.github.com>
2026-02-15 15:10:56 +0300

    Add bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Bot iko online ✅")
+
+bot.infinity_polling()
